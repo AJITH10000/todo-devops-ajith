f4c4ff396708d8870b4392193b0e561e5a1f6487
ajith <koppalaajithkumarreddy>
2026-02-06 19:13:56 +0000

    fix: make tests pipeline compatible

--- Backend/todo-summary-assistant/src/test/java/com/todoapp/TodoSummaryAssistantApplicationTests.java
+++ Backend/todo-summary-assistant/src/test/java/com/todoapp/TodoSummaryAssistantApplicationTests.java
@@ -3,11 +3,11 @@ package com.todoapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = TodoSummaryAssistantApplication.class)
 class TodoSummaryAssistantApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
+
